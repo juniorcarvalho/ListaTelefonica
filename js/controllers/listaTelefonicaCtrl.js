efb83187f32da455f34cfe8d5ed2f7028bd69726
Júnior Carvalho <joseadolfojr@gmail.com>
2017-01-05 21:22:39 -0200

    urls

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -6,20 +6,24 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
     var carregarContatos = function() {
         $http.get("https://listatelefonica-teste.herokuapp.com/contato/").then(function(data) {
             $scope.contatos = data;
+            console.log('carregou contatos');
         }).catch(function(response) {
             $scope.message = "Aconteceu um problema: " + response;
+            console.log(response);
         }).finally(function() {
-            console.log('carregou contatos');
+
         });
     };
 
     var carregarOperadoras = function() {
         $http.get("https://listatelefonica-teste.herokuapp.com/operadora/").then(function(data) {
             $scope.operadoras = data;
+            console.log('carregou operadoras');
         }).catch(function(response) {
             $scope.message = "Aconteceu um problema: " + response;
+            console.log(response);
         }).finally(function() {
-            console.log('carregou operadoras');
+
         });
     };
 
@@ -29,11 +33,13 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
         $http.post("https://listatelefonica-teste.herokuapp.com/contato/", contato).then(function(data) {
             delete $scope.contato;
             $scope.contatoForm.$setPristine();
+            console.log('gravou contato');
             carregarContatos();
         }).catch(function(response) {
             $scope.message = "Aconteceu um problema: " + response;
+            console.log(response)
         }).finally(function() {
-            console.log('carregou operadoras');
+            
         });
     };
 
